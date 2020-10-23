d70ca21f1832804727bd2511612adc4212fd3896
jamiegyoung <jamiegyoung22@gmail.com>
2020-10-24 00:45:46 +0100

    Added test socket to react js

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import './App.css';
-import Game from "./components/Game/Game"
-import StartMenu from './components/StartMenu/StartMenu'
+import "./App.css";
+import Game from "./components/Game/Game";
+import StartMenu from "./components/StartMenu/StartMenu";
+
+import socketIOClient from "socket.io-client";
 
 
 function App() {
+  useEffect(() => {
+    const socket = socketIOClient('http://localhost:4516/');
+    console.log(socket);
+    // socket.on("")
+  }, [])
   return (
     // This allows for routing the jsx based off the url
     <Router>
